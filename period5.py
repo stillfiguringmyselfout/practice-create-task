476395f5891e38f66eb5aa0e9eca1c2212260266
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2023-09-12 20:13:09 +0000

    Period 5 In Class Practice Task

--- period5.py
+++ period5.py
@@ -1,1 +1,26 @@
+restaurants = ["Daisy Mint", "Carl's Jr.", "Melt It!", "Taco Bell"]
 
+new_res = input("What restaurant would you like to add to the list?")
+
+def rank_restaurant(new_res, restaurants):
+
+    for i in range(len(restaurants)):
+
+        rank = input("Do you like A) " + new_res + " more or B) " + restaurants[i] + " more? A/B")
+
+        if rank == "A":
+
+            restaurants.insert(i, new_res)
+
+            break
+
+        elif rank == "B":
+
+            continue
+
+    if new_res not in restaurants:
+        restaurants.append(new_res)
+
+    return restaurants
+
+print("Your new ranking of restaurants is ", rank_restaurant(new_res, restaurants))
