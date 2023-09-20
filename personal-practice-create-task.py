ff78453d8f80b5817f16974e47a1a280855dfff0
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2023-09-20 00:05:27 +0000

    Added comments

--- personal-practice-create-task.py
+++ personal-practice-create-task.py
@@ -1,5 +1,12 @@
+#This is the dictionary used to define the terms for the search.
+
 state_fish_list = {"Hawaii": "rectangular triggerfish" , "California": "garibaldi", "The Moon": "anglerfish", "Colorado": "greenback cutthroat trout"}
 
+
+
+# This function takes in the dictionary, the search question (q), the message to be printed before the results,
+# and the message to be printed between the search term and the definition associated with it. It works
+# with any dictionary.
 
 def define(dictionary, q, printmsg1, printmsg2):
 
@@ -12,5 +19,9 @@ def define(dictionary, q, printmsg1, printmsg2):
       c = input("Search again? y/n ")
     else:
       print("ERROR")
+
+
+
+#This is the function call for state_fish_list, complete with the prompt and print messages.
 
 define(state_fish_list, "Enter state: ", "The state fish of", "is the")
